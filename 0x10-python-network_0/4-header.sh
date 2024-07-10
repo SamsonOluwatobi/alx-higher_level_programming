81b37ae09813a645222aaf1c5e3b281660781759
Samson <samtob2002@gmail.com>
2024-07-10 13:15:46 +0100

    Details changed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to get the body of a response from a server who checks a custom HTTP header in the request.
-curl -s "$1" -H 'X-HolbertonSchool-User-Id:98'
+curl -s "$1" -H 'X-School-User-Id:98'
